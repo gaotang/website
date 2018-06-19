fee0e1ed8267cbfe2244b8c1853459583b2d9195
gaotang <qq23745038@126.com>
2018-06-19 11:15:00 +0800

    初始化项目

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  title: "MogoH5+",
+  description: "快速开发WebApp的多页面脚手架工具",
+  base: "/mogo-h5plus/",
+  themeConfig: {
+    nav: [
+      { text: "主页", link: "/" },
+      { text: "MogoH5+文档", link: "/guide.md" },
+      { text: "上手指南", link: "/debug.md" },
+      { text: "H5+文档", link: "http://www.html5plus.org/doc/h5p.html" }
+    ],
+    repo: "tyaqing/mogo-h5plus",
+    search: true
+  }
+};
